b0db670cd5f611d0ab8e88ac8b98ca339822783b
Olena Hryhorzhevska <elenahry8@gmail.com>
2025-09-02 17:04:16 -0400

    Image component

--- app/(public routes)/profile/edit/page.tsx
+++ app/(public routes)/profile/edit/page.tsx
@@ -1,5 +1,12 @@
 // app/profile/edit/page.tsx
 
+import { Metadata } from "next";
+
+  export const metadata: Metadata = {
+    title: 'Edit Profile',
+    description: 'Edit your user details and settings',
+  };
+
 const EditProfile = () => {
   return (
     <div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Image from 'next/image';
+
 export default function Page() {
   return (
     <main>
       <h1>Welcome Home!</h1>
+      <p>This is the home page</p>
+      <Image src="/test.png" alt="test" width={400} height={300} />
+      <Image
+        src="https://picsum.photos/seed/picsum/300/300"
+        alt="test"
+        width={300}
+        height={300}
+      />
     </main>
   );
 }
